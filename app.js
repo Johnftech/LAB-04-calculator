bc00cd34a29f11bd2d208514ae11f10bedca8669
Johnftech <johnf.la.tech@gmail.com>
2021-10-07 13:37:13 -0700

    subtract UI, test, and variable declarations

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 // import functions
-import { add } from './calculations.js';
+import { add, subtract } from './calculations.js';
 // reference needed DOM elements
 console.log(add(50, 120));
+//addition
 const addNumber1 = document.getElementById('add-number-1');
 const addNumber2 = document.getElementById('add-number-2');
 const addButton = document.getElementById('add-button');
 const addAnswer = document.getElementById('add-answer');
-
+// subtraction
+const subNumber1 = document.getElementById('sub-number-1');
+const subNumber2 = document.getElementById('sub-number-2');
+const subButton = document.getElementById('sub-button');
+const subAnswer = document.getElementById('sub-answer');
 
 // set event listeners 
 //event listener goes on button
@@ -16,6 +21,8 @@ addButton.addEventListener('click', () => {
     const sum = add(num1, num2);
     addAnswer.textContent = sum;
 });
+
+
     // get user input(s)
     // do any needed work with the value(s)
     // update DOM to reflect new value(s)
--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,5 @@
 // IMPORT MODULES under test here:
-import { add } from '../calculations.js';
+import { add, subtract } from '../calculations.js';
 
 const test = QUnit.test;
 
@@ -30,11 +30,29 @@ test('adds two numbers 4 and 3', (expect) => {
 
     //2. Act 
     // Call the function you're testing and set the result to a const
+    const actual = subtract(x, y); // use your function here
+
+    //3. Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+//subtract
+test('subtracts two numbers 5 and 4', (expect) => {
+    //1. Arrange
+    // Set up your arguments and expectations
+    const x = 5;
+    const y = 4;
+    const expected = 1;
+
+    //2. Act 
+    // Call the function you're testing and set the result to a const
     const actual = add(x, y); // use your function here
 
     //3. Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
+
 });
 
 //export functions
