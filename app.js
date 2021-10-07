adfad3908f3517c50bcf83366890ae77601054e5
Johnftech <johnf.la.tech@gmail.com>
2021-10-07 13:09:46 -0700

    js adding function to reflect in DOM

--- app.js
+++ app.js
@@ -2,8 +2,20 @@
 import { add } from './calculations.js';
 // reference needed DOM elements
 console.log(add(50, 120));
+const addNumber1 = document.getElementById('add-number-1');
+const addNumber2 = document.getElementById('add-number-2');
+const addButton = document.getElementById('add-button');
+const addAnswer = document.getElementById('add-answer');
+
 
 // set event listeners 
+//event listener goes on button
+addButton.addEventListener('click', () => {
+    const num1 = +addNumber1.value;
+    const num2 = +addNumber2.value;
+    const sum = add(num1, num2);
+    addAnswer.textContent = sum;
+});
     // get user input(s)
     // do any needed work with the value(s)
     // update DOM to reflect new value(s)
